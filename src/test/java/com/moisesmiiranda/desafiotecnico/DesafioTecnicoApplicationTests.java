5a70be84a87d63ef326fffe5b6d7e999e2e12690
moisesmiiranda <moisesmiranda1994@gmail.com>
2021-02-28 22:09:15 -0300

    Iniciando o projeto

---
+++ src/test/java/com/moisesmiiranda/desafiotecnico/DesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moisesmiiranda.desafiotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
